60966828d9c0c88b23b150f9e58307541ca3fd7d
Peter Toth <peter.toth@firma.seznam.cz>
2021-01-09 14:01:47 +0100

    New package.

--- hello2.go
+++ hello2.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/xtothp00/hello2/morestrings"
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
 	fmt.Printf("%s\n", morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World!", "Hello Go!"))
 }
